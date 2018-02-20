72e859508f40800ada1c54642a3e4795025baadd
njmyers <17010801+njmyers@users.noreply.github.com>
2018-02-20 12:42:51 -0500

    added subtitle to manifest template

--- scripts/post-build.js
+++ scripts/post-build.js
@@ -6,8 +6,12 @@ const manifestBuildPath = path.join(process.cwd(), 'build', 'manifest.json');
 
 const built = require(manifestBuildPath);
 
+const name = process.env.REACT_APP_SITE_SUBTITLE
+	? `${process.env.REACT_APP_SITE_TITLE}: ${process.env.REACT_APP_SITE_SUBTITLE}`
+	: process.env.REACT_APP_SITE_TITLE;
+
 const merge = {
-	name: process.env.REACT_APP_SITE_TITLE,
+	name: `${process.env.REACT_APP_SITE_TITLE}: ${process.env.REACT_APP_SITE_SUBTITLE}`,
 	short_name: process.env.REACT_APP_SITE_TITLE,
 	description: process.env.REACT_APP_SITE_DESCRIPTION,
 	orientation: process.env.REACT_APP_ORIENTATION,
