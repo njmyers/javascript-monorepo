0507c57aa808e06a0e2221dedae09524feca6e77
njmyers <17010801+njmyers@users.noreply.github.com>
2018-02-21 13:18:03 -0500

    updated urlquery with lifecycle methods and listen to history method

--- src/components/URLQuery.js
+++ src/components/URLQuery.js
@@ -1,13 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { withRouter } from 'react-router-dom';
 import { parse } from 'qs';
 
 function URLQuery(Wrapped) {
-	function Search(props) {
-		const search = parse(props.history.location.search, { ignoreQueryPrefix: true });
-		return <Wrapped search={search} {...props} />;
-	}
+	class Search extends Component {
+		constructor(props) {
+			super(props);
+			this.handleHistoryChange = this.handleHistoryChange.bind(this);
 
+			const search = parse(this.props.location.search, { ignoreQueryPrefix: true });
+			this.state = { search };
+		}
+
+		handleHistoryChange(location) {
+			const search = parse(location.search, { ignoreQueryPrefix: true });
+			this.setState({ search });
+		}
+
+		componentDidMount() {
+			this.unlistenHistory = this.props.history.listen(this.handleHistoryChange);
+		}
+
+		componentWillUnmount() {
+			this.unlistenHistory();
+		}
+
+		render() {
+			return <Wrapped search={this.state.search} {...this.props} />;
+		}
+	}
 	return withRouter(Search);
 }
 
