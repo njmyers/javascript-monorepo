f40dee1b7ca61cc94d6305603334fec532eae5f4
njmyers <17010801+njmyers@users.noreply.github.com>
2018-03-10 16:09:37 -0500

    removed return statement footer container

--- src/components/Footer/FooterContainer.js
+++ src/components/Footer/FooterContainer.js
@@ -2,18 +2,14 @@ import { connect } from 'react-redux';
 import FooterComponent from './FooterComponent';
 import { updateContentSize } from '../Root/ui-sizing-actions';
 
-const mapStateToProps = (state, ownProps) => {
-	return {
-		audioPlayer: state.audioPlayer,
-		audioPlayerSize: state.UI.audioPlayerSize,
-	};
-};
+const mapStateToProps = (state, ownProps) => ({
+	audioPlayer: state.audioPlayer,
+	audioPlayerSize: state.UI.audioPlayerSize,
+});
 
-const mapDispatchToProps = (dispatch, ownProps) => {
-	return {
-		updateContentSize: (size) => dispatch(updateContentSize(size, 'footerSize')),
-	};
-};
+const mapDispatchToProps = (dispatch, ownProps) => ({
+	updateContentSize: (size) => dispatch(updateContentSize(size, 'footerSize')),
+});
 
 const Footer = connect(mapStateToProps, mapDispatchToProps)(FooterComponent);
 
