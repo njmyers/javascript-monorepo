2adf37974c791ed241e0104f90a7e3e923ec99eb
njmyers <17010801+njmyers@users.noreply.github.com>
2018-03-10 15:34:34 -0500

    moved async compose to a new package

--- src/utils/async/compose-async.js
+++
@@ -1,5 +0,0 @@
-import pipeAsync from './pipe-async';
-
-const composeAsync = (...fns) => pipeAsync(...fns.reverse());
-
-export default composeAsync;
--- src/utils/async/pipe-async.js
+++
@@ -1,5 +0,0 @@
-const pipeAsync = (fn, ...fns) => async (...args) => {
-	return !fns.length ? await fn(...args) : pipeAsync(...fns)(await fn(...args));
-};
-
-export default pipeAsync;
