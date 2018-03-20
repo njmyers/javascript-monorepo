7e0effb45af97a23c2864d03cc1dafd974e3703d
njmyers <17010801+njmyers@users.noreply.github.com>
2018-03-20 18:09:30 -0400

    created fill with and fill from for creating arrays

--- src/array/__tests__/fill-from.test.js
+++ src/array/__tests__/fill-from.test.js
@@ -1,7 +1,5 @@
 import fillFrom from '../fill-from';
 
-// console.log(fillFrom);
-
 test('fill array with sequence', () => {
 	const expected = [0, 1, 2, 3, 4];
 	const filled = fillFrom(0, 4);
--- src/array/index.js
+++ src/array/index.js
@@ -1,4 +1,3 @@
-export { default as createArray } from './create-array';
 export { default as fillFrom } from './fill-from';
-export { default as fill } from './fill';
+export { default as fillWith } from './fill-with';
 export { default as uniq } from './uniq';
