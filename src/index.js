fc91c1e16578e201f736f129952b86a2f5d797d7
njmyers <17010801+njmyers@users.noreply.github.com>
2018-03-21 15:23:59 -0400

    first commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import IFrameAPI from './IFrameAPI';
+import CreatePlayer from './CreatePlayer';
+import __ from 'smalldash';
+
+// specifies default null component if none is provided
+const createYouTube = (Component = (props) => null) =>
+	_.compose(IFrameAPI, CreatePlayer)(Component);
+
+export default createYouTube;
