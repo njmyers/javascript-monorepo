042e3ed404e3f7b660f1a962c275af5bdb225b15
njmyers <17010801+njmyers@users.noreply.github.com>
2018-05-24 11:15:32 -0400

    added camel to pascal case

--- src/string/__tests__/camel.test.js
+++ src/string/__tests__/camel.test.js
@@ -2,6 +2,7 @@ import camelToSentence from '../camel-to-sentence';
 import camelToKebab from '../camel-to-kebab';
 import camelToSnake from '../camel-to-snake';
 import camelToTitle from '../camel-to-title';
+import camelToPascal from '../camel-to-pascal';
 
 test('camel string to sentence case', () => {
 	const string = 'thisIsSomeCamel';
@@ -31,9 +32,17 @@ test('camel string to snake case', () => {
 	expect(camelToSnake(string)).toBe(expected);
 });
 
+test('camel string to pascal case', () => {
+	const string = 'thisIsSomeCamel';
+	const expected = 'ThisIsSomeCamel';
+
+	expect(camelToPascal(string)).toBe(expected);
+});
+
 test('fails on non string input', () => {
 	expect(() => camelToKebab(9)).toThrow();
 	expect(() => camelToSentence(9)).toThrow();
 	expect(() => camelToSnake(9)).toThrow();
 	expect(() => camelToTitle(9)).toThrow();
+	expect(() => camelToPascal(9)).toThrow();
 });
