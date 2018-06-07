1979d97d65e58a6bb9a5288c3caa9b593757c054
njmyers <17010801+njmyers@users.noreply.github.com>
2018-06-07 18:54:53 -0400

    added StatusSwitch with tests

---
+++ src/Library/StatusSwitch/Spinner.js
@@ -0,0 +1,14 @@
+// @flow
+import * as React from 'react';
+
+import './spinner-default.sass';
+
+const Spinner = (props: {}) => {
+  return (
+    <div className="statusSwitch_loadingContainer">
+      <i className="fa fa-spinner fa-spin fa-5x fa-fw" />
+    </div>
+  );
+};
+
+export default Spinner;
---
+++ src/Library/StatusSwitch/StatusSwitch.js
@@ -0,0 +1,44 @@
+// @flow
+import * as React from 'react';
+import Spinner from './Spinner';
+import ErrorMessage from './ErrorMessage';
+
+type Props = {
+  loading: React.ComponentType<{}>,
+  error: React.ComponentType<{}>,
+  status: string,
+  children?: React.Node,
+};
+
+class StatusSwitch extends React.Component<Props> {
+  static defaultProps = {
+    error: ErrorMessage,
+    loading: Spinner,
+    status: 'initial',
+    children: null,
+  };
+
+  cascadeStatus = (status: string | Array<string>) => {
+    if (!Array.isArray(status)) return status;
+    else if (status.includes('error')) return 'error';
+    else if (status.includes('loading')) return 'loading';
+    else if (status.includes('resolved')) return 'resolved';
+  };
+
+  render() {
+    switch (this.cascadeStatus(this.props.status)) {
+      case 'loading':
+        const Spinner: React.ComponentType<{}> = this.props.loading;
+        return <Spinner />;
+      case 'error':
+        const Error: React.ComponentType<{}> = this.props.error;
+        return <Error />;
+      case 'resolved':
+      case 'initial':
+      default:
+        return this.props.children;
+    }
+  }
+}
+
+export default StatusSwitch;
