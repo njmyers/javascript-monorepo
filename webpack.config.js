e791cb35cfd1f33eee81bcdf2db3cbe8d25568db
njmyers <17010801+njmyers@users.noreply.github.com>
2018-06-08 16:18:27 -0400

    tweaked build process and dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -1,57 +1,50 @@
 const path = require('path');
 
 module.exports = {
-    entry: './src/index.js',
-    devtool: 'source-map',
-    output: {
-        path: path.resolve(__dirname, 'build'),
-        filename: 'index.js',
-        libraryTarget: 'commonjs2', // THIS IS THE MOST IMPORTANT LINE! :mindblow: I wasted more than 2 days until realize this was the line most important in all this guide.
+  entry: './src/index.js',
+  devtool: 'source-map',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'index.js',
+    libraryTarget: 'commonjs2', // THIS IS THE MOST IMPORTANT LINE! :mindblow: I wasted more than 2 days until realize this was the line most important in all this guide.
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        include: path.resolve(__dirname, 'src'),
+        exclude: /(node_modules|build)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['env'],
+            plugins: ['transform-class-properties'],
+          },
+        },
+      },
+    ],
+  },
+  externals: {
+    react: {
+      root: 'React',
+      commonjs2: 'react',
+      commonjs: 'react',
+      amd: 'react',
+      umd: 'react',
     },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                include: path.resolve(__dirname, 'src'),
-                exclude: /(node_modules|build)/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['env'],
-                        plugins: ['transform-class-properties'],
-                    },
-                },
-            },
-        ],
+    'react-dom': {
+      root: 'ReactDOM',
+      commonjs2: 'react-dom',
+      commonjs: 'react-dom',
+      amd: 'react-dom',
+      umd: 'react-dom',
     },
-    externals: {
-        react: {
-            root: 'React',
-            commonjs2: 'react',
-            commonjs: 'react',
-            amd: 'react',
-            umd: 'react',
-        },
-        'react-dom': {
-            root: 'ReactDOM',
-            commonjs2: 'react-dom',
-            commonjs: 'react-dom',
-            amd: 'react-dom',
-            umd: 'react-dom',
-        },
-        lodash: {
-            root: '_',
-            commonjs2: 'lodash',
-            commonjs: 'lodash',
-            amd: 'lodash',
-            umd: 'lodash',
-        },
-        smalldash: {
-            root: '__',
-            commonjs2: 'smalldash',
-            commonjs: 'smalldash',
-            amd: 'smalldash',
-            umd: 'smalldash',
-        },
+    'react-children-utilities': {
+      root: 'Children',
+      commonjs2: 'react-children-utilities',
+      commonjs: 'react-children-utilities',
+      amd: 'react-children-utilities',
+      umd: 'react-children-utilities',
     },
+  },
 };
