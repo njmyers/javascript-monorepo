19239894129d46546e39169873ef8ec672ebd141
njmyers <17010801+njmyers@users.noreply.github.com>
2018-07-11 18:49:44 -0400

    working on updated storybook configuration

---
+++ .storybook/Layout.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Layout = ({ children }) => <article>{children}</article>;
+
+export default Layout;
--- .storybook/config.js
+++ .storybook/config.js
@@ -1,7 +1,16 @@
-import { configure } from '@storybook/react';
+import * as React from 'react';
+import * as storybook from '@storybook/react';
+// import { withInfo } from '@storybook/addon-info';
+import docs from 'storybook-addon-docgen';
 import { setOptions } from '@storybook/addon-options';
-
+import { withKnobs } from '@storybook/addon-knobs/react';
+// components
+import Layout from './Layout';
+// styles
 import 'normalize.css';
+
+// Add Global Knobs
+storybook.addDecorator(withKnobs);
 
 setOptions({
   name: 'React Component Library',
@@ -11,18 +20,24 @@ setOptions({
   showAddonPanel: true,
   showSearchBox: false,
   addonPanelInRight: true,
-  sortStoriesByKind: true,
+  sortStoriesByKind: false,
+});
+
+storybook.addDecorator((stories, context) => {
+  return <Layout>{stories()}</Layout>;
 });
 
 // automatically import all files ending in *.stories.js
 const req = require.context(
-  '../packages/component-library',
+  '../packages/component-library/src',
   true,
   /.stories.js$/
 );
 
-function loadStories() {
+// webpack voodoo
+const loadStories = () => {
   req.keys().forEach((filename) => req(filename));
-}
+};
 
-configure(loadStories, module);
+// initialize storybook
+storybook.configure(loadStories, module);
