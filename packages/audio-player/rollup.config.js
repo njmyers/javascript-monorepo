3653b7276aade615ce38b0f9615118f488dde3fa
njmyers <17010801+njmyers@users.noreply.github.com>
2018-07-13 19:28:14 -0400

    basic config for breaking out components

---
+++ packages/audio-player/rollup.config.js
@@ -0,0 +1,33 @@
+import resolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
+import babel from 'rollup-plugin-babel';
+import pkg from './package.json';
+
+export default [
+  {
+    input: 'src/index.js',
+    external: ['react', 'react-dom', 'smalldash', 'lodash-es'],
+    output: {
+      name: 'withSize',
+      file: pkg.browser,
+      format: 'umd',
+      sourcemap: true,
+      globals: {
+        react: 'React',
+        'react-dom': 'ReactDOM',
+        smalldash: '__',
+        lodash: '_',
+      },
+    },
+    plugins: [resolve(), babel({ exclude: 'node_modules/**' }), commonjs()],
+  },
+  {
+    input: 'src/index.js',
+    external: ['react', 'react-dom', 'smalldash', 'lodash-es', 'invariant'],
+    output: [
+      { file: pkg.main, format: 'cjs', sourcemap: true },
+      { file: pkg.module, format: 'es', sourcemap: true },
+    ],
+    plugins: [resolve(), babel({ exclude: 'node_modules/**' })],
+  },
+];
