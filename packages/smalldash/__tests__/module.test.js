3572a72ce56d1a15c76e5284f49768ecd1a0dd59
njmyers <17010801+njmyers@users.noreply.github.com>
2018-07-20 19:04:51 -0400

    fixed module test

--- packages/smalldash/__tests__/module.test.js
+++ packages/smalldash/__tests__/module.test.js
@@ -1,4 +1,4 @@
-import * as library from '../dist/module';
+import * as library from '../build/smalldash.cjs.js';
 
 test('library exports correctly', () => {
   expect(library).toHaveProperty('compose');
