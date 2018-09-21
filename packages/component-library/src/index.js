505e5e34ffe788b13292ba5bf44222c5103f732a
njmyers <17010801+njmyers@users.noreply.github.com>
2018-09-21 16:34:29 -0400

    fixed index file

--- packages/component-library/src/index.js
+++ packages/component-library/src/index.js
@@ -8,5 +8,5 @@ export { default as Loading } from './Loading';
 export { default as Icon } from './Icon';
 export { default as LazyImage } from './LazyImage';
 export { default as Modal } from './Modal';
-export createTransitionStyle from './utils/create-transition-style';
+export { default as createTransitionStyle } from './utils/create-transition-style';
 export * from './Modal';
