780de8533e17cffc9985f58c2fd9a3cdc9dd84f3
njmyers <17010801+njmyers@users.noreply.github.com>
2019-06-26 20:07:06 -0400

    Inherit from master rollup config

--- packages/directory/rollup.config.js
+++ packages/directory/rollup.config.js
@@ -1,62 +1,21 @@
-import resolve from 'rollup-plugin-node-resolve';
-import builtins from 'rollup-plugin-node-builtins';
-import commonjs from 'rollup-plugin-commonjs';
-import babel from 'rollup-plugin-babel';
-import runtimes from '@njmyers/babel-runtime-files';
+/* eslint-disable */
+import { es6, cjs } from '@njmyers/rollup-config';
 import pkg from './package.json';
 
-const external = [
-  ...Object.keys(pkg.dependencies),
-  ...runtimes(),
-  'fs',
-  'path',
-];
+const external = ['fs', 'path'];
+
+const cjsConfig = cjs(pkg);
+const es6Config = es6(pkg);
 
 export default [
   {
-    input: 'src/index.js',
-    external,
-    output: {
-      file: pkg.main,
-      format: 'cjs',
-      sourcemap: true,
-    },
-    plugins: [
-      resolve({
-        jsnext: true,
-        main: true,
-      }),
-      builtins(),
-      babel({
-        runtimeHelpers: true,
-        exclude: 'node_modules/**',
-        plugins: ['@babel/plugin-transform-runtime'],
-      }),
-    ],
+    input: 'src/index.ts',
+    ...cjsConfig,
+    external: [...cjsConfig.external, ...external],
   },
   {
-    input: 'src/index.js',
-    external,
-    output: {
-      file: pkg.module,
-      format: 'es',
-      sourcemap: true,
-    },
-    plugins: [
-      resolve(),
-      builtins(),
-      babel({
-        runtimeHelpers: true,
-        exclude: 'node_modules/**',
-        plugins: [
-          [
-            '@babel/plugin-transform-runtime',
-            {
-              useESModules: true,
-            },
-          ],
-        ],
-      }),
-    ],
+    input: 'src/index.ts',
+    ...cjsConfig,
+    external: [...cjsConfig.external, ...external],
   },
 ];
