3ef6e1d5ef3d55c702053d3c717bbea879849242
njmyers <17010801+njmyers@users.noreply.github.com>
2019-07-04 14:09:57 -0400

    Update config

--- packages/component-library/rollup.config.js
+++ packages/component-library/rollup.config.js
@@ -1,58 +1,13 @@
-import resolve from 'rollup-plugin-node-resolve';
-import commonjs from 'rollup-plugin-commonjs';
-import babel from 'rollup-plugin-babel';
-import runtimes from '@njmyers/babel-runtime-files';
+import { es6, cjs } from '@njmyers/rollup-config';
 import pkg from './package.json';
-
-const external = [
-  ...Object.keys(pkg.dependencies),
-  ...Object.keys(pkg.peerDependencies),
-  ...runtimes(),
-];
 
 export default [
   {
-    input: 'src/index.js',
-    external,
-    output: {
-      file: pkg.main,
-      format: 'cjs',
-      sourcemap: true,
-    },
-    plugins: [
-      resolve({
-        jsnext: true,
-        main: true,
-      }),
-      babel({
-        runtimeHelpers: true,
-        exclude: 'node_modules/**',
-        plugins: ['@babel/plugin-transform-runtime'],
-      }),
-    ],
+    ...cjs(pkg),
+    input: 'src/index.ts',
   },
   {
-    input: 'src/index.js',
-    external,
-    output: {
-      file: pkg.module,
-      format: 'es',
-      sourcemap: true,
-    },
-    plugins: [
-      resolve(),
-      babel({
-        runtimeHelpers: true,
-        exclude: 'node_modules/**',
-        plugins: [
-          [
-            '@babel/plugin-transform-runtime',
-            {
-              useESModules: true,
-            },
-          ],
-        ],
-      }),
-    ],
+    ...es6(pkg),
+    input: 'src/index.ts',
   },
 ];
