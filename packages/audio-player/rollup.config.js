68c13721d0fd17bddfacbea06c1e295b2c1d0d57
njmyers <17010801+njmyers@users.noreply.github.com>
2019-07-04 14:18:32 -0400

    Uppdate story paths

--- packages/audio-player/rollup.config.js
+++ packages/audio-player/rollup.config.js
@@ -1,71 +1,20 @@
-import resolve from 'rollup-plugin-node-resolve';
-import commonjs from 'rollup-plugin-commonjs';
-import babel from 'rollup-plugin-babel';
-import runtimes from '@njmyers/babel-runtime-files';
-import postcss from 'rollup-plugin-postcss';
-import autoprefixer from 'autoprefixer';
+import { es6, cjs } from '@njmyers/rollup-config';
 import pkg from './package.json';
 
-const external = [
-  ...Object.keys(pkg.dependencies),
-  ...Object.keys(pkg.peerDependencies),
-  ...runtimes(),
-  'uuid/v1',
-];
+const external = ['uuid/v1'];
+
+const cjsConfig = cjs(pkg);
+const es6Config = es6(pkg);
 
 export default [
   {
-    input: 'src/index.js',
-    external,
-    output: {
-      file: pkg.main,
-      format: 'cjs',
-      sourcemap: true,
-    },
-    plugins: [
-      resolve({
-        jsnext: true,
-        main: true,
-      }),
-      postcss({
-        plugins: [autoprefixer],
-        extract: 'build/style.css',
-        sourceMap: true,
-      }),
-      babel({
-        runtimeHelpers: true,
-        exclude: 'node_modules/**',
-        plugins: ['@babel/plugin-transform-runtime'],
-      }),
-    ],
+    input: 'src/index.ts',
+    ...cjsConfig,
+    external: [...cjsConfig.external, ...external],
   },
   {
-    input: 'src/index.js',
-    external,
-    output: {
-      file: pkg.module,
-      format: 'es',
-      sourcemap: true,
-    },
-    plugins: [
-      resolve(),
-      babel({
-        runtimeHelpers: true,
-        exclude: 'node_modules/**',
-        plugins: [
-          [
-            '@babel/plugin-transform-runtime',
-            {
-              useESModules: true,
-            },
-          ],
-        ],
-      }),
-      postcss({
-        plugins: [autoprefixer],
-        extract: 'build/style.css',
-        sourceMap: true,
-      }),
-    ],
+    input: 'src/index.ts',
+    ...cjsConfig,
+    external: [...cjsConfig.external, ...external],
   },
 ];
