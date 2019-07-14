de3a11bc0fb8194d17e7d333f733578e9e5fe491
njmyers <17010801+njmyers@users.noreply.github.com>
2019-07-14 17:35:48 -0400

    Remove uuid from dependencies

--- packages/audio-player/rollup.config.js
+++ packages/audio-player/rollup.config.js
@@ -1,20 +1,13 @@
 import { es6, cjs } from '@njmyers/rollup-config';
 import pkg from './package.json';
 
-const external = ['uuid/v1'];
-
-const cjsConfig = cjs(pkg);
-const es6Config = es6(pkg);
-
 export default [
   {
     input: 'src/index.ts',
-    ...cjsConfig,
-    external: [...cjsConfig.external, ...external],
+    ...cjs(pkg),
   },
   {
     input: 'src/index.ts',
-    ...cjsConfig,
-    external: [...cjsConfig.external, ...external],
+    ...es6(pkg),
   },
 ];
