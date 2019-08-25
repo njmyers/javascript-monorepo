bb9ca1fcf63df99f3a62bb876cdfed30af8a55ef
njmyers <17010801+njmyers@users.noreply.github.com>
2019-08-24 21:28:26 -0400

    Add default options

---
+++ packages/boil/src/defaults.js
@@ -0,0 +1,10 @@
+import path from "path";
+
+const defaults = {
+  templatesDirectory: path.resolve(__dirname, "../templates"),
+  extension: undefined,
+  include: undefined,
+  exclude: undefined
+};
+
+export default defaults;
