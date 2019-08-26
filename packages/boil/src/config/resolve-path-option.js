aad4d273d4e81409a5caf61c72bc05cfbed00774
njmyers <17010801+njmyers@users.noreply.github.com>
2019-08-25 22:35:28 -0400

    Refactor to accomodate rcConfig and cli args

--- packages/boil/src/config/resolve-path-option.js
+++ packages/boil/src/config/resolve-path-option.js
@@ -3,15 +3,14 @@ import path from 'path';
 /**
  * Resolve a path that is given as a configuration value. If the path is an
  * absolute path then we simply use the absolute path. If it is a relative path
- * then we resolve the path relative to the location of the .boilrc file that
- * specified the path
+ * then we resolve the path relative to the location that specified the path
  */
-function resolvePathOption(rcFilePath, pathOption) {
-  if (!pathOption || (!rcFilePath && !pathOption)) {
+function resolvePathOption(pathOptionLocation, pathOption) {
+  if (!pathOption || (!pathOptionLocation && !pathOption)) {
     return '';
   }
 
-  if (!rcFilePath) {
+  if (!pathOptionLocation) {
     return pathOption;
   }
 
@@ -19,7 +18,7 @@ function resolvePathOption(rcFilePath, pathOption) {
     return pathOption;
   }
 
-  return path.resolve(path.dirname(rcFilePath), pathOption);
+  return path.resolve(pathOptionLocation, pathOption);
 }
 
 export default resolvePathOption;
