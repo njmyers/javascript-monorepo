86909d6453ec8d54df1403ac203d8208c89c4cf4
njmyers <17010801+njmyers@users.noreply.github.com>
2019-08-25 22:52:41 -0400

    Remove unused function

--- packages/boil/src/utils/index.js
+++ packages/boil/src/utils/index.js
@@ -1,4 +1,3 @@
 export { default as replaceTemplateStrings } from './replace-template-strings';
-export { default as validateArgs } from './validate-args';
 export { default as templateTokens } from './template-tokens';
 export { default as removeLeadingSlash } from './remove-leading-slash';
