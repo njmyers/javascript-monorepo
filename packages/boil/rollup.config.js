ff32e0b5de29db3a85e07b4f860a632f72488304
njmyers <17010801+njmyers@users.noreply.github.com>
2019-08-25 23:17:32 -0400

    Prettify

--- packages/boil/rollup.config.js
+++ packages/boil/rollup.config.js
@@ -1,58 +1,58 @@
-import path from "path";
-import resolve from "rollup-plugin-node-resolve";
-import builtins from "rollup-plugin-node-builtins";
-import commonjs from "rollup-plugin-commonjs";
-import babel from "rollup-plugin-babel";
-import json from "rollup-plugin-json";
-import runtimes from "@njmyers/babel-runtime-files";
-import pkg from "./package.json";
+import path from 'path';
+import resolve from 'rollup-plugin-node-resolve';
+import builtins from 'rollup-plugin-node-builtins';
+import commonjs from 'rollup-plugin-commonjs';
+import babel from 'rollup-plugin-babel';
+import json from 'rollup-plugin-json';
+import runtimes from '@njmyers/babel-runtime-files';
+import pkg from './package.json';
 
 const external = [
   ...Object.keys(pkg.dependencies),
   ...runtimes(),
-  "fs",
-  "path"
+  'fs',
+  'path',
 ];
 
 const config = {
   external,
   output: {
     file: pkg.main,
-    format: "cjs",
-    sourcemap: true
+    format: 'cjs',
+    sourcemap: true,
   },
   plugins: [
     resolve({
       jsnext: true,
-      main: true
+      main: true,
     }),
     json(),
     builtins(),
     babel({
       runtimeHelpers: true,
-      exclude: "node_modules/**",
-      plugins: ["@babel/plugin-transform-runtime"]
-    })
-  ]
+      exclude: 'node_modules/**',
+      plugins: ['@babel/plugin-transform-runtime'],
+    }),
+  ],
 };
 
 export default [
   {
     ...config,
-    input: "src/program.js",
+    input: 'src/program.js',
     output: {
       file: pkg.module,
-      format: "esm",
-      sourcemap: true
-    }
+      format: 'esm',
+      sourcemap: true,
+    },
   },
   {
     ...config,
-    input: "src/program.js",
+    input: 'src/program.js',
     output: {
       file: pkg.main,
-      format: "cjs",
-      sourcemap: true
-    }
-  }
+      format: 'cjs',
+      sourcemap: true,
+    },
+  },
 ];
