210d177b0b7e51c819affb2dcd8fbb5ffaac965d
njmyers <17010801+njmyers@users.noreply.github.com>
2019-08-25 21:22:33 -0400

    Use different token format and add additional tokens

--- packages/boil/src/utils/template-tokens.js
+++ packages/boil/src/utils/template-tokens.js
@@ -2,22 +2,30 @@ import { kebabToTitle, kebabToCamel, kebabToSnake } from 'smalldash';
 
 const templateTokens = [
   {
-    token: 'camel',
+    token: 'camelCase',
     fn: kebabToCamel,
   },
   {
-    token: 'Pascal',
+    token: 'PascalCase',
     fn: string =>
       kebabToTitle(string)
         .split(' ')
         .join(''),
   },
   {
-    token: 'kebab',
+    token: 'kebab-case',
     fn: string => string,
   },
   {
-    token: 'UPPER',
+    token: 'KEBAB-CASE',
+    fn: string => string.toUpperCase(),
+  },
+  {
+    token: 'snake_case',
+    fn: kebabToSnake,
+  },
+  {
+    token: 'SNAKE_CASE',
     fn: string => kebabToSnake(string).toUpperCase(),
   },
 ];
