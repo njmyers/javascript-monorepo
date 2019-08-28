df4221dea968f321b50cac68d42559722a78d6cd
njmyers <17010801+njmyers@users.noreply.github.com>
2019-08-27 20:28:10 -0400

    Fix template for function test

--- packages/boil/templates/function/__tests__/kebab-case.test.js
+++ packages/boil/templates/function/__tests__/kebab-case.test.js
@@ -2,7 +2,7 @@ import camelCase from '../kebab-case';
 
 describe('it works', () => {
   test('it is a function ', () => {
-    expect(typeof camelCase()).toBe('function');
+    expect(typeof camelCase).toBe('function');
   });
 
   test('it runs without crashing', () => {
