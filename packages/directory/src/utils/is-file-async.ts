9dd17265e9691423a52cf1d5d93c8c110253808c
njmyers <17010801+njmyers@users.noreply.github.com>
2019-09-07 19:03:51 -0400

    Add is file async function

--- packages/directory/src/utils/is-file-async.ts
+++ packages/directory/src/utils/is-file-async.ts
@@ -1,8 +1,19 @@
+import fs from 'fs';
+
 /**
- *
+ * Use the file system to check asynchronously whether a path is a file or a
+ * directory and return a promise of the result.
  */
-function isFileAsync() {
-  return null;
+function isFileAsync(file: string): Promise<boolean> {
+  return new Promise((resolve, reject) => {
+    fs.stat(file, (err, stats) => {
+      if (err) {
+        return reject(err);
+      }
+
+      resolve(stats.isFile());
+    });
+  });
 }
 
 export default isFileAsync;
