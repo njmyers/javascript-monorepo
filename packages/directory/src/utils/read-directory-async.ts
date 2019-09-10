4857766da0f270a9d40c67087ddeb44763644537
njmyers <17010801+njmyers@users.noreply.github.com>
2019-09-09 21:54:37 -0400

    Add read directory sync and async versions

--- packages/directory/src/utils/read-directory-async.ts
+++ packages/directory/src/utils/read-directory-async.ts
@@ -1,8 +1,53 @@
+import fs from 'fs';
+import path from 'path';
+import { promisify } from 'util';
+import { pipeAsync } from 'smalldash';
+
+import isFileAsync from './is-file-async';
+import flatten from './flatten';
+import { Options } from '../types';
+
+const readdirAsync = promisify(fs.readdir);
+
 /**
- *
+ * Asynchronously read a directory and returns an array of all of the paths. Use
+ * the recursive option to read a directory up to a specified depth. If you
+ * input relative paths you will receive relative paths. If you input absolute
+ * paths you will receive absolute paths.
  */
-function readDirectoryAsync() {
-  return null;
+async function readDirectoryAsync(
+  directory: string,
+  options: Options = {}
+): Promise<string[]> {
+  if (await isFileAsync(directory)) {
+    return [directory];
+  }
+
+  return await pipeAsync(
+    readdirAsync,
+    (directoryContents: string[]) =>
+      Promise.all(
+        directoryContents.map(async (name: string) => {
+          const filePath = path.resolve(directory, name);
+          const isFile = await isFileAsync(filePath);
+
+          if (options.recursive && isFile) {
+            return filePath;
+          }
+
+          if (options.recursive && !isFile) {
+            return await readDirectoryAsync(filePath, options);
+          }
+
+          if (isFile) {
+            return filePath;
+          }
+
+          return null;
+        })
+      ),
+    flatten
+  )(directory, 'utf8');
 }
 
 export default readDirectoryAsync;
--- packages/directory/src/utils/read-directory-sync.ts
+++ packages/directory/src/utils/read-directory-sync.ts
@@ -1,29 +1,49 @@
 import fs from 'fs';
-import isFileSync from './is-file-sync';
+import path from 'path';
 
+import isFileSync from './is-file-sync';
+import flatten from './flatten';
 import { Options } from '../types';
 
 /**
- * Reads a directory and returns an array of their paths.
- * If you input relative paths you will receive relative paths.
- * If you input absolute paths you will receive absolute paths.
+ * Synchronously read a directory and returns an array of all of the paths. Use
+ * the recursive option to read a directory up to a specified depth. If you
+ * input relative paths you will receive relative paths. If you input absolute
+ * paths you will receive absolute paths.
  */
-function readDirectorySync(dir: string, options: Options = {}): string[] {
-  return isFileSync(dir)
-    ? [dir]
-    : []
-        .concat(
-          ...fs.readdirSync(dir).map(name => {
-            const filePath = `${dir}/${name}`;
-            return options.recursive
-              ? isFileSync(filePath)
-                ? filePath
-                : readDirectorySync(filePath, options)
-              : isFileSync(filePath)
-              ? filePath
-              : '';
-          })
-        )
-        .filter(string => string);
+function readDirectorySync(directory: string, options: Options = {}): string[] {
+  if (isFileSync(directory)) {
+    return [directory];
+  }
+
+  const directoryContents = fs.readdirSync(directory, 'utf8').map(name => {
+    const filePath = path.resolve(directory, name);
+    const isFile = isFileSync(filePath);
+
+    if (options.recursive && isFile) {
+      return filePath;
+    }
+
+    if (options.recursive && !isFile) {
+      return readDirectorySync(filePath, options);
+    }
+
+    if (isFile) {
+      return filePath;
+    }
+
+    return null;
+  });
+
+  if (!directoryContents) {
+    return [];
+  }
+
+  if (!Array.isArray(directoryContents)) {
+    return [directoryContents].filter(Boolean);
+  }
+
+  return flatten(directoryContents).filter(Boolean);
 }
+
 export default readDirectorySync;
