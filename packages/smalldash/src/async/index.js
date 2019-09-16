d13ab8ade574c038dd9420eaaa17c5159238e293
njmyers <17010801+njmyers@users.noreply.github.com>
2019-09-15 21:26:59 -0400

    Convert to typescript files

--- packages/smalldash/src/async/index.js
+++
@@ -1,7 +0,0 @@
-/** @flow */
-export { default as composeAsync } from './compose-async';
-export { default as debounce } from './debounce';
-export { default as pipeAsync } from './pipe-async';
-export { default as throttle } from './throttle';
-
-export * from './types'
---
+++ packages/smalldash/src/async/index.ts
@@ -0,0 +1,4 @@
+export { default as composeAsync } from './compose-async'
+export { default as debounce } from './debounce'
+export { default as pipeAsync } from './pipe-async'
+export { default as throttle } from './throttle'
