ea47e8157039b44a9b30296b5f30d5e4450f5894
njmyers <17010801+njmyers@users.noreply.github.com>
2019-09-15 21:27:32 -0400

    Convert to typescript files

---
+++ packages/smalldash/src/functional/effect.ts
@@ -0,0 +1,14 @@
+/**
+ * This function allows a side effect to be put into a chain of functional
+ * arguments. This is useful for debugging or developing in a compositional
+ * functional style. The effect will run using a single argument and will return
+ * the same argument after running the effect.
+ */
+function effect(effectFunction) {
+  return arg => {
+    effectFunction(arg);
+    return arg;
+  };
+}
+
+export default effect;
