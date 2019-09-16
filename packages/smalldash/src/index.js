58e1d1ebd22d774b949d3e3f525ab6132ba1f91e
njmyers <17010801+njmyers@users.noreply.github.com>
2019-09-15 21:27:45 -0400

    Convert to typescript files

--- packages/smalldash/src/index.js
+++
@@ -1,6 +0,0 @@
-export * from './array';
-export * from './async';
-export * from './functional';
-export * from './object';
-export * from './string';
-export * from './primitives';
---
+++ packages/smalldash/src/index.ts
@@ -0,0 +1,6 @@
+export * from './array'
+export * from './async'
+export * from './functional'
+export * from './object'
+export * from './primitives'
+export * from './string'
