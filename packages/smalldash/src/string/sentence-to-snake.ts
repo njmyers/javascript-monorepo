1a2914986b138cb2ed95234f712f46bed8fd3ce1
njmyers <17010801+njmyers@users.noreply.github.com>
2019-09-19 22:12:15 -0400

    Fix errors found from sentence tests

--- packages/smalldash/src/string/sentence-to-snake.ts
+++ packages/smalldash/src/string/sentence-to-snake.ts
@@ -1,12 +1,12 @@
 import caseToCase from './case-to-case';
-import { KEBAB_CASE_SEPARATOR, SNAKE_CASE_SEPARATOR } from './constants';
+import { SENTENCE_CASE_SEPARATOR, SNAKE_CASE_SEPARATOR } from './constants';
 
 /**
  * Convert a sentence case string to a snake_case string and trim all
  * whitespace.
  */
 function sentenceToSnake(sentenceCaseString) {
-  return caseToCase(SNAKE_CASE_SEPARATOR)(KEBAB_CASE_SEPARATOR)(
+  return caseToCase(SENTENCE_CASE_SEPARATOR)(SNAKE_CASE_SEPARATOR)(
     sentenceCaseString
   );
 }
--- packages/smalldash/src/string/sentence-to-title.ts
+++ packages/smalldash/src/string/sentence-to-title.ts
@@ -1,16 +1,10 @@
-import caseToCase from './case-to-case';
-import { KEBAB_CASE_SEPARATOR, SENTENCE_CASE_SEPARATOR } from './constants';
-import compose from '../functional/compose';
 import capitalize from './capitalize';
 
 /**
  * Convert a sentence case string to Title Case string and trim all whitespace.
  */
-function kebabToSentence(sentenceCaseString) {
-  return compose(
-    capitalize,
-    caseToCase(SENTENCE_CASE_SEPARATOR)(KEBAB_CASE_SEPARATOR)
-  )(sentenceCaseString);
+function sentenceToTitle(sentenceCaseString) {
+  return capitalize(sentenceCaseString);
 }
 
-export default kebabToSentence;
+export default sentenceToTitle;
