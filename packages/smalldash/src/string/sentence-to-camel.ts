5811addafd11020865fe1361dc230ea4482fb3f9
njmyers <17010801+njmyers@users.noreply.github.com>
2019-09-19 21:14:01 -0400

    Update comment

--- packages/smalldash/src/string/sentence-to-camel.ts
+++ packages/smalldash/src/string/sentence-to-camel.ts
@@ -2,8 +2,7 @@ import toCamel from './to-camel';
 import { SENTENCE_CASE_SEPARATOR } from './constants';
 
 /**
- * Convert a kebab-case string to a sentence case string and trim all
- * whitespace.
+ * Convert a sentence case string to a camelCase string and trim all whitespace.
  */
 function sentenceToCamel(kebabCaseString) {
   return toCamel(SENTENCE_CASE_SEPARATOR)(kebabCaseString);
