cb2af221e54817a36f2f178e5b05e3caaed773da
njmyers <17010801+njmyers@users.noreply.github.com>
2019-09-19 20:59:42 -0400

    Update comments

--- packages/smalldash/src/string/camel-to.ts
+++ packages/smalldash/src/string/camel-to.ts
@@ -3,6 +3,7 @@ import isUpperCase from './is-upper-case';
 /**
  * Create a camelCase converter function. This can be used to insert separator
  * characters where words were previously delimited by upper case characters.
+ * This function will also trim all whitespace before and after the string.
  */
 function camelTo(separator) {
   return string =>
