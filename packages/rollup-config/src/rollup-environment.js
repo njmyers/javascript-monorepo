3e305f53f8d93d260e5b56c599e8e9a19c98aaf9
njmyers <17010801+njmyers@users.noreply.github.com>
2019-10-05 15:43:21 -0400

    Add lodash merge

--- packages/rollup-config/src/rollup-environment.js
+++ packages/rollup-config/src/rollup-environment.js
@@ -4,40 +4,46 @@ const commonjs = require('rollup-plugin-commonjs');
 const babel = require('rollup-plugin-babel');
 const runtimes = require('@njmyers/babel-runtime-files');
 const json = require('rollup-plugin-json');
+const _ = require('lodash');
 const { node, postcss, typescript, flow } = require('./environments');
 
 const defaultOptions = {
   /** default rollup options */
-  external: undefined,
-  format: 'cjs',
+  external: runtimes(),
+  output: {
+    format: 'cjs',
+    sourcemap: true,
+  },
   /** environment settings */
   env: ['browser', 'typescript'],
   /** plugin options */
-  babel: undefined,
-  typescript: undefined,
-  flow: undefined,
-  json: undefined,
-  commonjs: undefined,
-  resolve: undefined,
+  babel: {
+    runtimeHelpers: true,
+    exclude: 'node_modules/**',
+    extensions: ['.js', '.jsx', '.ts', '.tsx'],
+    plugins: ['@babel/plugin-transform-runtime'],
+  },
+  typescript: {},
+  flow: {},
+  json: {},
+  commonjs: {},
+  resolve: {
+    preferBuiltins: true,
+  },
 };
 
 const rollupEnvironment = (pkg, userOptions = {}) => {
-  const options = {
-    ...defaultOptions,
-    ...userOptions,
-  };
+  const options = _.merge(defaultOptions, userOptions);
 
   const config = {
     input: options.input,
     external: [
       ...Object.keys(pkg.dependencies || {}),
       ...Object.keys(pkg.peerDependencies || {}),
-      ...runtimes(),
       ...(options.external || []),
     ],
     output: {
       file: options.output.format === 'es' ? pkg.module : pkg.main,
-      sourcemap: true,
       ...options.output,
     },
     plugins: [
@@ -48,13 +54,7 @@ const rollupEnvironment = (pkg, userOptions = {}) => {
       ...flow(options),
       ...postcss(options),
       ...node(options),
-      babel({
-        runtimeHelpers: true,
-        exclude: 'node_modules/**',
-        extensions: ['.js', '.jsx', '.ts', '.tsx'],
-        plugins: ['@babel/plugin-transform-runtime'],
-        ...options.babel,
-      }),
+      babel(options.babel),
     ],
   };
 
