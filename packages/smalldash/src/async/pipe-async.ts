eb8fd0c5e59fc45efa20307fb5857895a09262e7
njmyers <17010801+njmyers@users.noreply.github.com>
2020-01-04 20:55:52 -0500

    Add identity as default function

--- packages/smalldash/src/async/pipe-async.ts
+++ packages/smalldash/src/async/pipe-async.ts
@@ -1,11 +1,13 @@
 // @ts-nocheck
+import { identity } from '../functional';
 
 /**
  * Make sure to use async as function and not arrow function to prevent babel/rollup issues
  * https://github.com/rollup/rollup/issues/1518
  */
-const pipeAsync = (fn, ...fns) => async (...args): unknown => {
-  return !fns.length ? await fn(...args) : pipeAsync(...fns)(await fn(...args));
-};
+const pipeAsync = (fn = identity, ...fns) =>
+  async function(arg): unknown {
+    return !fns.length ? await fn(arg) : pipeAsync(...fns)(await fn(arg));
+  };
 
 export default pipeAsync;
