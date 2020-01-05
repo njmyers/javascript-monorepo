71d822abd98957074b6a7046c631dec247b85d6d
njmyers <17010801+njmyers@users.noreply.github.com>
2020-01-05 13:07:10 -0500

    Fix button types

--- packages/audio-player/src/Button/types.ts
+++ packages/audio-player/src/Button/types.ts
@@ -1,6 +1,8 @@
 import * as React from 'react';
 import { Props as IconProps } from '@fortawesome/react-fontawesome';
 
-export interface Props extends IconProps {
-  onClick: (e: React.MouseEvent<HTMLElement>, ...args: [any]) => void;
+interface ButtonProps {
+  onClick: (e: React.MouseEvent<HTMLButtonElement>, ...args: [any]) => void;
 }
+
+export type Props = IconProps & ButtonProps;
