cb34c22652c3ca10b133048d662018dcca01fe38
njmyers <17010801+njmyers@users.noreply.github.com>
2020-01-04 21:33:33 -0500

    Add ignore patterns

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,11 @@
 module.exports = {
   preset: 'ts-jest',
-  testEnvironment: 'node',
+  testPathIgnorePatterns: [
+    '/node_modules/',
+    '/helpers/',
+    '/templates/',
+    '__fixtures__',
+  ],
   globals: {
     'ts-jest': {
       diagnostics: false,
