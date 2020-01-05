f8ff94e091b8f4cccedeee644add77757b8c25d6
njmyers <17010801+njmyers@users.noreply.github.com>
2020-01-04 20:46:15 -0500

    Write better mocked tests for pipe and compose

--- packages/smalldash/src/functional/__tests__/compose.test.ts
+++ packages/smalldash/src/functional/__tests__/compose.test.ts
@@ -1,34 +1,83 @@
 import compose from '../compose';
 
-const increment = a => a + 1;
-const square = a => Math.pow(a, 2);
+const ARG = 0;
+const RETURN = 1;
 
 describe('functional/compose', () => {
-  test('it is a function', () => {
-    expect(typeof compose).toBe('function');
+  describe('type check', () => {
+    test('it is a function', () => {
+      expect(typeof compose).toBe('function');
+    });
+
+    test('it returns a function', () => {
+      expect(typeof compose()).toBe('function');
+    });
   });
 
-  test('it returns a function', () => {
-    expect(typeof compose()).toBe('function');
+  describe('no functions', () => {
+    test('it does not throw an error', () => {
+      expect(() => compose()).not.toThrow();
+    });
+
+    const noArguments = compose();
+
+    test('it does not throw an error when curried', () => {
+      expect(() => noArguments()).not.toThrow();
+    });
+
+    test('it defaults to identity function', () => {
+      expect(noArguments(ARG)).toBe(ARG);
+    });
   });
 
-  test('it composes together multiple functions', () => {
-    const addThree = compose(
-      increment,
-      increment,
-      increment
-    );
+  describe('single function', () => {
+    const fn = jest.fn();
+    const singleFunction = compose(fn);
 
-    expect(addThree(0)).toBe(3);
+    beforeEach(() => {
+      fn.mockClear();
+      fn.mockReturnValue(RETURN);
+      singleFunction(ARG);
+    });
+
+    test('it calls with the argument', () => {
+      expect(fn).toHaveBeenCalledWith(ARG);
+    });
+
+    test('it returns the value', () => {
+      expect(fn).toHaveReturnedWith(RETURN);
+    });
   });
 
-  test('it executes functions from right to left', () => {
-    const addTwoAndSquare = compose(
-      square,
-      increment,
-      increment
-    );
+  describe('multiple function', () => {
+    const fn1 = jest.fn();
+    const fn2 = jest.fn();
+    const fns = [fn1, fn2];
+    const multipleFunctions = compose(...fns);
 
-    expect(addTwoAndSquare(0)).toBe(4);
+    beforeEach(() => {
+      fns.forEach(fn => {
+        fn.mockClear();
+        fn.mockReturnValue(RETURN);
+      });
+
+      multipleFunctions(ARG);
+    });
+
+    test('it calls the second with the argument', () => {
+      expect(fn2).toHaveBeenCalledWith(ARG);
+    });
+
+    test('it calls the first with the return of the second', () => {
+      expect(fn1).toHaveBeenCalledWith(RETURN);
+    });
+
+    test('it returns the value', () => {
+      expect(fn1).toHaveReturnedWith(RETURN);
+    });
+
+    test('it returns the value', () => {
+      expect(fn2).toHaveReturnedWith(RETURN);
+    });
   });
 });
--- packages/smalldash/src/functional/__tests__/pipe.test.ts
+++ packages/smalldash/src/functional/__tests__/pipe.test.ts
@@ -1,34 +1,83 @@
 import pipe from '../pipe';
 
-const increment = a => a + 1;
-const square = a => Math.pow(a, 2);
+const ARG = 0;
+const RETURN = 1;
 
 describe('functional/pipe', () => {
-  test('it is a function', () => {
-    expect(typeof pipe).toBe('function');
+  describe('type check', () => {
+    test('it is a function', () => {
+      expect(typeof pipe).toBe('function');
+    });
+
+    test('it returns a function', () => {
+      expect(typeof pipe()).toBe('function');
+    });
   });
 
-  test('it returns a function', () => {
-    expect(typeof pipe()).toBe('function');
+  describe('no functions', () => {
+    test('it does not throw an error', () => {
+      expect(() => pipe()).not.toThrow();
+    });
+
+    const noArguments = pipe();
+
+    test('it does not throw an error when curried', () => {
+      expect(() => noArguments()).not.toThrow();
+    });
+
+    test('it defaults to identity function', () => {
+      expect(noArguments(ARG)).toBe(ARG);
+    });
   });
 
-  test('it pipes together multiple functions', () => {
-    const addThree = pipe(
-      increment,
-      increment,
-      increment
-    );
+  describe('single function', () => {
+    const fn = jest.fn();
+    const singleFunction = pipe(fn);
 
-    expect(addThree(0)).toBe(3);
+    beforeEach(() => {
+      fn.mockClear();
+      fn.mockReturnValue(RETURN);
+      singleFunction(ARG);
+    });
+
+    test('it calls with the argument', () => {
+      expect(fn).toHaveBeenCalledWith(ARG);
+    });
+
+    test('it returns the value', () => {
+      expect(fn).toHaveReturnedWith(RETURN);
+    });
   });
 
-  test('it executes functions from left to right', () => {
-    const addTwoAndSquare = pipe(
-      increment,
-      increment,
-      square
-    );
+  describe('multiple function', () => {
+    const fn1 = jest.fn();
+    const fn2 = jest.fn();
+    const fns = [fn1, fn2];
+    const multipleFunctions = pipe(...fns);
 
-    expect(addTwoAndSquare(0)).toBe(4);
+    beforeEach(() => {
+      fns.forEach(fn => {
+        fn.mockClear();
+        fn.mockReturnValue(RETURN);
+      });
+
+      multipleFunctions(ARG);
+    });
+
+    test('it calls the first with the argument', () => {
+      expect(fn1).toHaveBeenCalledWith(ARG);
+    });
+
+    test('it calls the second with the return of the first', () => {
+      expect(fn2).toHaveBeenCalledWith(RETURN);
+    });
+
+    test('it returns the value', () => {
+      expect(fn1).toHaveReturnedWith(RETURN);
+    });
+
+    test('it returns the value', () => {
+      expect(fn2).toHaveReturnedWith(RETURN);
+    });
   });
 });
