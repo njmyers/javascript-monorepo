54f065f9111814752e953267b4be5b38f679052d
njmyers <17010801+njmyers@users.noreply.github.com>
2020-01-04 20:59:54 -0500

    Use ts-jest

--- jest.config.js
+++ jest.config.js
@@ -1,20 +1,9 @@
-/* eslint-disable */
-const { defaults } = require('jest-config');
-
 module.exports = {
-  // collectCoverage: true,
-  collectCoverageFrom: [
-    'packages/**/*.{js}',
-    '!**/node_modules/**',
-    '!**/flow-typed/**',
-  ],
-  roots: ['packages/'],
-  moduleNameMapper: {
-    '\\.(jpg|jpeg|png|svg|woff|woff2)$': '<rootDir>/__mocks__/fileMock.js',
-    '\\.(sass|css|scss)$': '<rootDir>/__mocks__/styleMock.js',
-    '\\.(md)$': '<rootDir>/__mocks__/markdown-mock.js',
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  globals: {
+    'ts-jest': {
+      diagnostics: false,
+    },
   },
-  testPathIgnorePatterns: ['/node_modules/'],
-  transformIgnorePatterns: ['<rootDir>.*(node_modules)(?!.*smalldash.*).*$'],
-  moduleFileExtensions: [...defaults.moduleFileExtensions, 'ts', 'tsx'],
 };
