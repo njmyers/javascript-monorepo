f12ef4bbc5df5fe4bfb0f7a9d1eca1e0beeb7d34
njmyers <17010801+njmyers@users.noreply.github.com>
2020-03-25 20:41:32 -0400

    Use one assertion per test

--- packages/smalldash/src/primitives/__tests__/objectish.test.ts
+++ packages/smalldash/src/primitives/__tests__/objectish.test.ts
@@ -13,43 +13,55 @@ describe('primitives/objectish', () => {
     expect(objectish(Object.create(null))).toBe(true);
   });
 
-  test('it returns true for classes', () => {
-    class Null {
-      constructor(value) {
-        return;
-      }
-    }
-    expect(objectish(new Null())).toBe(true);
+  test('it returns true for instantiated classes', () => {
+    expect(objectish(new (class {})())).toBe(true);
   });
 
   test('it returns false for null', () => {
     expect(objectish(null)).toBe(false);
   });
 
-  test('it returns false for functions', () => {
+  test('it returns false for arrow functions', () => {
     expect(objectish(() => null)).toBe(false);
-    expect(
-      objectish(function() {
-        return null;
-      })
-    ).toBe(false);
+  });
+
+  test('it returns false for functions defined with the keyword', () => {
+    expect(objectish(function() {})).toBe(false);
   });
 
   test('it returns false for arrays', () => {
     expect(objectish([])).toBe(false);
   });
 
-  test('it returns false for truthy values', () => {
+  test('it returns false for truthy string values', () => {
     expect(objectish('string')).toBe(false);
+  });
+
+  test('it returns false for truthy number values', () => {
     expect(objectish(10)).toBe(false);
+  });
+
+  test('it returns false for falsey number values', () => {
+    expect(objectish(0)).toBe(false);
+  });
+
+  test('it returns false for NaN', () => {
+    expect(objectish(NaN)).toBe(false);
+  });
+
+  test('it returns false for "true" boolean value', () => {
     expect(objectish(true)).toBe(false);
   });
 
-  test('it returns false for other falsey values', () => {
+  test('it returns false for "false" boolean value', () => {
     expect(objectish(false)).toBe(false);
-    expect(objectish(0)).toBe(false);
-    expect(objectish(NaN)).toBe(false);
+  });
+
+  test('it returns false for null', () => {
     expect(objectish(null)).toBe(false);
+  });
+
+  test('it returns false for "undefined"', () => {
     expect(objectish(undefined)).toBe(false);
   });
 });
--- packages/smalldash/src/primitives/numberish.ts
+++ packages/smalldash/src/primitives/numberish.ts
@@ -1,4 +1,3 @@
-// @ts-nocheck
 import { Complex } from './types';
 
 /**
