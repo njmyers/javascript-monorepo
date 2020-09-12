c0c6cee2032fde6e2fefd6374f39937b88097feb
njmyers <17010801+njmyers@users.noreply.github.com>
2020-09-12 12:54:36 -0400

    Improve types and add overloads

--- packages/directory/src/utils/flatten.ts
+++ packages/directory/src/utils/flatten.ts
@@ -1,12 +1,42 @@
-// @ts-nocheck
-
 /**
- * Flatten a nested array up to a given depth.
+ * Flatten a nested array of items up to infinite depth.
+ *
+ * @param array - A possibly nested array of items
+ * @returns A non-nested array of items
  */
-function flatten(array: unknown[], depth = Infinity, currentDepth = 0): any[] {
-  return Array.isArray(array)
-    ? [].concat(...array.map(thing => flatten(thing, depth, currentDepth + 1)))
-    : [].concat(array);
+function flatten<T>(array: T): T[];
+
+/** Overload for an array of depth one */
+function flatten<T>(array: T[]): T[];
+
+/** Overload for an array of depth two */
+function flatten<T>(array: T[][]): T[];
+
+/** Overload for an array of depth three */
+function flatten<T>(array: T[][][]): T[];
+
+/** Overload for an array of depth four */
+function flatten<T>(array: T[][][][]): T[];
+
+/** Overload for an array of depth five */
+function flatten<T>(array: T[][][][][]): T[];
+
+/** Overload for an array of depth six */
+function flatten<T>(array: T[][][][][][]): T[];
+
+/** Implementation */
+function flatten<T>(array: T): T[] {
+  const flattened: T[] = [];
+
+  if (!Array.isArray(array)) {
+    return flattened.concat(array);
+  }
+
+  array.forEach(item => {
+    flattened.push(...flatten<T>(item));
+  });
+
+  return flattened;
 }
 
 export default flatten;
