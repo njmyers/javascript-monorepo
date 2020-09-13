b9a01ba9c921133299d6e20b009185e47687c78e
njmyers <17010801+njmyers@users.noreply.github.com>
2020-09-13 17:41:03 -0400

    Remove image-server package

--- packages/image-server/app.js
+++
@@ -1,7 +0,0 @@
-const server = require('./build/app.cjs.js');
-
-server({
-  port: 3030,
-  hostname: 'localhost',
-  debug: false,
-});
--- packages/image-server/src/sharp/log-sharp-flow.ts
+++
@@ -1,9 +0,0 @@
-import { tap } from 'rxjs/operators';
-
-export const startSharpTimer = tap((ctx) => {
-  console.time('sharp');
-});
-
-export const endSharpTimer = tap((ctx) => {
-  console.timeEnd('sharp');
-});
