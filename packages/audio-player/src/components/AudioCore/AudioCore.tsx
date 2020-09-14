1c6ec27a327671212e630ac3e78dc918e297db8d
njmyers <17010801+njmyers@users.noreply.github.com>
2020-09-13 21:18:22 -0400

    Add nocheck and disable declaration

--- packages/audio-player/src/components/AudioCore/AudioCore.tsx
+++ packages/audio-player/src/components/AudioCore/AudioCore.tsx
@@ -1,3 +1,4 @@
+// @ts-nocheck
 import * as React from 'react';
 import withSize from 'react-size-components';
 // assets
--- packages/audio-player/src/components/Button/Button.tsx
+++ packages/audio-player/src/components/Button/Button.tsx
@@ -1,3 +1,4 @@
+// @ts-nocheck
 import * as React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
