1eb8aaaea77c591f69c48564c5159052f9ede251
njmyers <17010801+njmyers@users.noreply.github.com>
2020-10-18 21:08:28 -0400

    Add equal checking of props

--- packages/react-you-tube/src/use-you-tube.ts
+++ packages/react-you-tube/src/use-you-tube.ts
@@ -1,12 +1,20 @@
-import { useEffect, useState } from 'react';
+import * as React from 'react';
+import isEqual from 'lodash.isequal';
 import loadYouTubeAPI from './load-you-tube-api';
 // types
 import { Props } from './types';
 
 function useYouTube(node: HTMLElement | null, props: Props): YT.Player | null {
-  const [player, setPlayer] = useState<YT.Player | null>(null);
+  const [safeProps, setSafeProps] = React.useState(props);
+  const [player, setPlayer] = React.useState<YT.Player | null>(null);
 
-  useEffect(() => {
+  React.useEffect(() => {
+    if (!isEqual(props, safeProps)) {
+      setSafeProps(props);
+    }
+  }, [props, safeProps]);
+
+  React.useEffect(() => {
     const load = async (): Promise<void> => {
       if (player) {
         return;
@@ -18,12 +26,12 @@ function useYouTube(node: HTMLElement | null, props: Props): YT.Player | null {
 
       try {
         const YT = await loadYouTubeAPI();
-        void new YT.Player(node, {
+        new YT.Player(node, {
+          ...safeProps,
           events: {
             onReady: (e): void => setPlayer(e.target),
-            ...(props.events || {}),
+            ...safeProps.events,
           },
-          ...props,
         });
       } catch (e) {
         setPlayer(null);
@@ -31,7 +39,7 @@ function useYouTube(node: HTMLElement | null, props: Props): YT.Player | null {
     };
 
     void load();
-  }, [player, node, props]);
+  }, [node, player, safeProps]);
 
   return player;
 }
